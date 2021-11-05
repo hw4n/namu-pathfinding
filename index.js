94a5accd2ae1fef1c2e6dc971f8a9519ae6f6cf1
Hwanhee Chae <hh@hwan.me>
2021-11-05 18:20:38 +0900

    feat: add namuDFS and namuBFS

--- index.js
+++ index.js
@@ -12,5 +12,52 @@ await mongoose.connect(process.env.DBURI).then(() => {
 import { getNamuDocumentByTitle } from './helper/db.js';
 import { getEveryLinkFromText } from './helper/namu.js';
 
-const { text } = await getNamuDocumentByTitle('JavaScript');
-console.log(getEveryLinkFromText(text));
+const visited = new Set();
+
+// not very efficient but it works
+async function namuDFS(currentTitle, destTitle, path=[]) {
+  if (visited.has(currentTitle)) {
+    return;
+  }
+  path.push(currentTitle);
+  visited.add(currentTitle);
+
+  if (currentTitle === destTitle) {
+    console.log(path);
+    process.exit(0);
+  }
+
+  const namuDocument = await getNamuDocumentByTitle(currentTitle);
+  if (namuDocument === null) {
+    return;
+  }
+  const { text } = namuDocument;
+  const links = getEveryLinkFromText(text);
+  for (const link of links) {
+    namuDFS(link, destTitle, path);
+  }
+}
+
+// yeah this works well
+async function namuBFS(startTitle, destTitle) {
+  const queue = [[startTitle, [startTitle]]];
+  while (queue.length) {
+    const [currentTitle, path] = queue.shift();
+    if (visited.has(currentTitle)) {
+      continue;
+    }
+    const namuDocument = await getNamuDocumentByTitle(currentTitle);
+    if (namuDocument === null) {
+      continue;
+    }
+    const { text } = namuDocument;
+    const links = getEveryLinkFromText(text);
+    for (const link of links) {
+      if (link === destTitle) {
+        console.log([...path, link]);
+        process.exit(0);
+      }
+      queue.push([link, [...path, link]]);
+    }
+  }
+}
